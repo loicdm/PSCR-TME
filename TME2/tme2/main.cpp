3af548ec2fc032b4b5126d0b68d9fb85c2722fe5
Loïc Daudé Mondet <loic.daude@mondet.fr>
2023-09-27 17:16:20 +0200

    Nombre d'occurrences par mots

--- TME2/tme2/main.cpp
+++ TME2/tme2/main.cpp
@@ -13,10 +13,9 @@ int main () {
 	auto start = steady_clock::now();
 	cout << "Parsing War and Peace" << endl;
 
-	size_t nombre_lu = 0;
 	// prochain mot lu
 	string word;
-    vector<string> word_list;
+    vector<pair<string,int>> word_list;
 
 	// une regex qui reconnait les caractères anormaux (négation des lettres)
 	regex re( R"([^a-zA-Z])");
@@ -26,19 +25,22 @@ int main () {
 		// passe en lowercase
 		transform(word.begin(),word.end(),word.begin(),::tolower);
         bool found = false;
-        for (const string& unique_word: word_list) {
-            if (unique_word==word) {
+        for (pair<string,int>& unique_word: word_list) {
+            if (unique_word.first==word) {
                 found = true;
+                unique_word.second++;
                 break;
             }
         }
         if (!found) {
-            word_list.push_back(word);
+            pair<string,int> word_pair;
+            word_pair.first = word;
+            word_pair.second = 1;
+            word_list.emplace_back(word_pair);
             // word est maintenant "tout propre"
-            if (nombre_lu % 100 == 0)
+            if (word_list.size() % 100 == 0)
                 // on affiche un mot "propre" sur 100
-                cout << nombre_lu << ": "<< word << endl;
-            nombre_lu++;
+                cout << word_list.size() << ": "<< word << endl;
         }
 
 
@@ -52,8 +54,24 @@ int main () {
               << duration_cast<milliseconds>(end - start).count()
               << "ms.\n";
 
-    cout << "Found a total of " << nombre_lu << " unique words." << endl;
-
+    cout << "Found a total of " << word_list.size() << " unique words." << endl;
+    size_t war_occ = 0;
+    size_t peace_occ = 0;
+    size_t toto_occ = 0;
+    for (pair<string,int>& unique_word: word_list) {
+        if (unique_word.first=="war") {
+            war_occ=unique_word.second;
+        } else
+        if (unique_word.first=="peace") {
+            peace_occ=unique_word.second;
+        } else
+        if (unique_word.first=="toto") {
+            toto_occ=unique_word.second;
+        }
+    }
+    cout << "Found a total of " << war_occ << " occurrences of word war" << endl;
+    cout << "Found a total of " << peace_occ << " occurrences of word peace." << endl;
+    cout << "Found a total of " << toto_occ << " occurrences of word toto." << endl;
     return 0;
 }
 
