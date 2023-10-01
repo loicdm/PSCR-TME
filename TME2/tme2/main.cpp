52d445c5150a3963f9eb1b5fcf902cb837801884
Loïc Daudé Mondet <loic.daude@mondet.fr>
2023-10-01 21:59:19 +0200

    utilisation de l'unordered_map de la lib standard

--- TME2/tme2/main.cpp
+++ TME2/tme2/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <regex>
 #include <chrono>
-#include "HashMap.h"
+#include <unordered_map>
 
 int main () {
 	using namespace std;
@@ -16,7 +16,7 @@ int main () {
 
 	// prochain mot lu
 	string word;
-    pr::HashMap<string, int> word_map(256);
+    unordered_map<string, int> word_map(256);
     //vector<pair<string,int>> word_list;
 
 	// une regex qui reconnait les caractères anormaux (négation des lettres)
@@ -26,15 +26,15 @@ int main () {
 		word = regex_replace ( word, re, "");
 		// passe en lowercase
 		transform(word.begin(),word.end(),word.begin(),::tolower);
-        int * word_count = word_map.get(word);
-        if (!word_count) {
-            word_map.put(word, 1);
+        auto word_count = word_map.find(word);
+        if (word_count!= word_map.end()) {
+            word_map.emplace(word, 1);
             // word est maintenant "tout propre"
             if (word_map.size() % 100 == 0)
                 // on affiche un mot "propre" sur 100
                 cout << word_map.size() << ": "<< word << endl;
         } else {
-            word_map.put(word, *(word_count)+1);
+            word_map.emplace(word, word_count->second+1);
         }
 
 
@@ -43,15 +43,18 @@ int main () {
 
 	cout << "Finished Parsing War and Peace" << endl;
 
+   // std::vector<pair<string,int>> word_list = vector<pair<string,int>>
+
+
 	auto end = steady_clock::now();
     cout << "Parsing took "
               << duration_cast<milliseconds>(end - start).count()
               << "ms.\n";
 
     cout << "Found a total of " << word_map.size() << " unique words." << endl;
-    cout << "Found a total of " << (word_map.get("war") ?  *word_map.get("war") : 0) << " occurrences of word war" << endl;
-    cout << "Found a total of " << (word_map.get("peace") ?  *word_map.get("peace") : 0) << " occurrences of word peace." << endl;
-    cout << "Found a total of " << (word_map.get("toto") ?  *word_map.get("toto") : 0) << " occurrences of word toto." << endl;
+    cout << "Found a total of " << word_map.find("war")->second << " occurrences of word war" << endl;
+    cout << "Found a total of " << word_map.find("peace")->second << " occurrences of word peace." << endl;
+    cout << "Found a total of " << word_map.find("toto")->second << " occurrences of word toto." << endl;
     return 0;
 }
 
